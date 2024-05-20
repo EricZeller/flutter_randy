dfc3e0ec518896b3811bf1c8786aa169f8bcec53
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-05-20 21:12:15 +0200

    material design added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'dart:math';
 
 void main() => runApp(const RandomNumberGeneratorApp());
@@ -8,26 +9,50 @@ class RandomNumberGeneratorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(fontFamily: 'Poppins'),
-          bodyMedium: TextStyle(fontFamily: 'Poppins'),
-          titleLarge: TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w500),
-          // Füge weitere Textstile hinzu, falls benötigt
-        ),
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(fontFamily: 'Poppins'),
-          bodyMedium: TextStyle(fontFamily: 'Poppins'),
-          titleLarge: TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w500),
-          // Füge weitere Textstile hinzu, falls benötigt
-        ),
-      ),
-      themeMode: ThemeMode.system,
-      home: const RandomNumberGeneratorScreen(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          lightColorScheme = lightDynamic.harmonized();
+          darkColorScheme = darkDynamic.harmonized();
+        } else {
+          lightColorScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
+          darkColorScheme = ColorScheme.fromSeed(
+              seedColor: Colors.blue, brightness: Brightness.dark);
+        }
+
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightColorScheme,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(fontFamily: 'Poppins'),
+              bodyMedium: TextStyle(fontFamily: 'Poppins'),
+              bodySmall: TextStyle(fontFamily: 'Poppins'),
+              titleLarge: TextStyle(fontFamily: 'Poppins'),
+              titleMedium: TextStyle(fontFamily: 'Poppins'),
+              titleSmall: TextStyle(fontFamily: 'Poppins'),
+            ),
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkColorScheme,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(fontFamily: 'Poppins'),
+              bodyMedium: TextStyle(fontFamily: 'Poppins'),
+              bodySmall: TextStyle(fontFamily: 'Poppins'),
+              titleLarge: TextStyle(fontFamily: 'Poppins'),
+              titleMedium: TextStyle(fontFamily: 'Poppins'),
+              titleSmall: TextStyle(fontFamily: 'Poppins'),
+            ),
+          ),
+          themeMode: ThemeMode.system,
+          home: const RandomNumberGeneratorScreen(),
+        );
+      },
     );
   }
 }
@@ -62,12 +87,8 @@ class _RandomNumberGeneratorScreenState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 117, 65, 126),
-        title: const Center(
-            child: Text(
-          'Randy',
-          style: TextStyle(fontSize: 32),
-        )),
+        title: Center(child: Text('Randy', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 36, color: Theme.of(context).colorScheme.onPrimaryContainer),)),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -79,7 +100,7 @@ class _RandomNumberGeneratorScreenState
                   child: TextField(
                     controller: _minController,
                     decoration: const InputDecoration(
-                      labelText: 'Min Value',
+                      labelText: 'Min',
                       border: OutlineInputBorder(),
                     ),
                     keyboardType: TextInputType.number,
@@ -90,7 +111,7 @@ class _RandomNumberGeneratorScreenState
                   child: TextField(
                     controller: _maxController,
                     decoration: const InputDecoration(
-                      labelText: 'Max Value',
+                      labelText: 'Max',
                       border: OutlineInputBorder(),
                     ),
                     keyboardType: TextInputType.number,
@@ -99,8 +120,11 @@ class _RandomNumberGeneratorScreenState
               ],
             ),
             const SizedBox(height: 20),
+            const Text('Default: 0-100'),
+            const SizedBox(height: 20),
             Text('Number of Random Numbers: $_numOfRandomNumbers'),
             Slider(
+              inactiveColor: Theme.of(context).colorScheme.onSecondary,
               value: _numOfRandomNumbers.toDouble(),
               min: 1,
               max: 100,
@@ -114,8 +138,10 @@ class _RandomNumberGeneratorScreenState
             ),
             const SizedBox(height: 20),
             ElevatedButton.icon(
-              onPressed: _generateRandomNumbers,
               icon: const Icon(Icons.shuffle),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.onTertiary),
+              onPressed: _generateRandomNumbers,
               label: const Text('Generate'),
             ),
             const SizedBox(height: 20),
@@ -125,8 +151,10 @@ class _RandomNumberGeneratorScreenState
                 itemBuilder: (context, index) {
                   return Card(
                     child: ListTile(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                       title: Text(
-                          'Random Number ${index + 1}: ${_randomNumbers[index]}'),
+                          'Random Number ${index + 1}: ${_randomNumbers[index]}', style: TextStyle(color: Theme.of(context).colorScheme.onSecondaryContainer),),
+                      tileColor: Theme.of(context).colorScheme.secondaryContainer,
                     ),
                   );
                 },
